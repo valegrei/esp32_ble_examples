d60f25787da1fbe845bf6c881eb182534e06fecb
valegrei <victoralegre@uni.pe>
2023-05-28 11:58:11 -0500

    pruebas

--- BleCustomServer/BleCustomServer.ino
+++ BleCustomServer/BleCustomServer.ino
@@ -23,6 +23,10 @@
 #include <BLEServer.h>
 #include <BLEUtils.h>
 #include <BLE2902.h>
+#include "EEPROM.h"
+
+int addr = 0;
+#define EEPROM_SIZE 64
 
 BLEServer *pServer = NULL;
 BLECharacteristic *infoCharacteristic;
@@ -31,7 +35,7 @@ BLECharacteristic *statusCharacteristic;
 bool deviceConnected = false;
 bool oldDeviceConnected = false;
 uint8_t txValue = 0;
-String dataReceived = "";
+std::string dataReceived = "";
 
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
@@ -44,7 +48,7 @@ String dataReceived = "";
 #define CHARACTERISTIC_INFO_UUID "bee6d633-72e2-4336-9199-faa484d14b95" // Lee metadatos del equipo
 #define CHARACTERISTIC_REQ_UUID "52491fd0-7498-4cd5-9cd6-b03c1d3b5272" // Recibe comandos
 #define CHARACTERISTIC_RES_UUID "64f316dc-0a57-49f0-9f48-9500a9009d93" // Responde a comandos
-#define CHARACTERISTIC_STATUS_UUID "bee6d633-72e2-4336-9199-faa484d14b95" // Notifica el estado de los sensores a tiempo real
+#define CHARACTERISTIC_STATUS_UUID "a12f4c9e-503b-45eb-8d7b-7fb774cf51d1" // Notifica el estado de los sensores a tiempo real
 
 /* This function handles the server callbacks */
 class MyServerCallbacks : public BLEServerCallbacks {
@@ -58,10 +62,36 @@ class MyServerCallbacks : public BLEServerCallbacks {
 };
 
 void printDataReceived() {
-  Serial.println("*********");
-  Serial.print("Received Value: ");
-  Serial.println(dataReceived);
-  Serial.println("*********");
+  if(dataReceived == "H-L"){
+    int f = readHour(1);
+    std::string msg1 = "H-L-ABCDEF";
+    std::string msg2 = "H-M-ABCDEF";
+    std::string msg3 = "H-K-ABCDEF";
+    std::string msg4 = "H-J-ABCDEF";
+    std::string msg5 = "H-V-ABCDEF";
+    std::string msg6 = "H-S-ABCDEF";
+    std::string msg7 = "H-D-ABCDEF";
+    std::string F = std::to_string(f);
+    resCharacteristic->setValue(msg1);
+    resCharacteristic->indicate();
+  }
+  if(dataReceived == "ON"){
+    resCharacteristic->setValue("ON");
+    resCharacteristic->indicate();
+    relay("ON");
+    Serial.println("ON RECIBIDO");
+  }
+    if(dataReceived == "OFF"){
+    resCharacteristic->setValue("OFF");
+    resCharacteristic->indicate();
+    relay("OFF");
+    Serial.println("OFF RECIBIDO");
+  }// Serial.println("*********");
+  // Serial.print("Received Value: ");
+  // Serial.println(dataReceived);
+  // Serial.println("*********");
+  //resCharacteristic->setValue(dataReceived);
+  //resCharacteristic->notify();
 }
 
 class MyCallbacks : public BLECharacteristicCallbacks {
@@ -73,21 +103,26 @@ class MyCallbacks : public BLECharacteristicCallbacks {
         if (inputValue[i] == '|') {
           printDataReceived();
           dataReceived = "";
+          break;
         } else {
           dataReceived = dataReceived + inputValue[i];
         }
       }
     }
   }
+  
 };
 
 
 
 void setup() {
   Serial.begin(115200);
-
+  EEPROMBEGIN();
+  pinMode(14,OUTPUT);
+  pinMode(33,OUTPUT);
+  digitalWrite(33,HIGH);
   // Create and name the BLE Device
-  BLEDevice::init("UART Service");
+  BLEDevice::init("Biosfera Biologica");
 
   // Create the BLE Server
   pServer = BLEDevice::createServer();
@@ -110,7 +145,7 @@ void setup() {
 
   resCharacteristic = dataService->createCharacteristic(
     CHARACTERISTIC_RES_UUID,
-    BLECharacteristic::PROPERTY_NOTIFY);
+    BLECharacteristic::PROPERTY_INDICATE);//PROPERTY_NOTIFY
 
   resCharacteristic->addDescriptor(new BLE2902());
 
@@ -133,9 +168,11 @@ void loop() {
 
   // Aqui recolectas los datos de los sensores y enviarlo con setValue, luego notificar
   if (deviceConnected) {
-    pTxCharacteristic->setValue(&txValue, 1);
-    pTxCharacteristic->notify();
-    txValue++;
+    int Battery = map(analogRead(34)*3.30/2048.00,0,3.20,0,100);
+    Serial.println(Battery);
+    statusCharacteristic->setValue(Battery);
+    statusCharacteristic->notify();
+    //txValue++;
     delay(10);  // bluetooth stack will go into congestion, if too many packets are sent
   }
 
@@ -151,4 +188,36 @@ void loop() {
     // do stuff here on connecting
     oldDeviceConnected = deviceConnected;
   }
+}
+
+
+void EEPROMBEGIN(){
+  
+  Serial.println("start...");
+  if (!EEPROM.begin(EEPROM_SIZE))
+  {
+    Serial.println("failed to initialise EEPROM"); delay(1000000);
+  }
+  Serial.println(" bytes read from Flash . Values are:");
+  for (int i = 0; i < EEPROM_SIZE; i++)
+  {
+    Serial.print(byte(EEPROM.read(i))); Serial.print(" ");
+  }
+  Serial.println();
+  Serial.println("writing random n. in memory");
+
+}
+
+uint8_t readHour(int addres){
+  uint8_t value = EEPROM.read(addres);
+  return value; 
+}
+void relay(String s){
+  if(s=="ON"){
+    digitalWrite(33,HIGH);
+  }
+  if(s=="OFF"){
+    digitalWrite(33,LOW);
+  }
+
 }
